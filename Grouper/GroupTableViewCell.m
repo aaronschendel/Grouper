ece5a0bd5d13ceb93219dc1aeef58e966ff2724c
Aaron <thecomwiz@gmail.com>
2015-03-09 21:24:58 -0500

    added groupviewcontroller and started implementing the tableview

---
+++ Grouper/GroupTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  GroupTableViewCell.m
+//  Grouper
+//
+//  Created by Aaron Schendel on 3/9/15.
+//  Copyright (c) 2015 Aaron. All rights reserved.
+//
+
+#import "GroupTableViewCell.h"
+
+@implementation GroupTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Grouper/ViewGroupsViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewGroupsViewController.h
+//  Grouper
+//
+//  Created by Aaron Schendel on 3/9/15.
+//  Copyright (c) 2015 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewGroupsViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic) NSInteger numOfGroups;
+
+
+
+@end
