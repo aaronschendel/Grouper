a94055da9b68f82a51c3171ceb54ae96e7a639f7
Aaron Schendel <aaron.schendel@gmail.com>
2015-04-22 18:35:15 -0500

    Working on group creation

--- Grouper/CreateGroupsViewController.m
+++ Grouper/CreateGroupsViewController.m
@@ -8,6 +8,7 @@
 
 #import "CreateGroupsViewController.h"
 #import "PersonList.h"
+#import "PersonListStore.h"
 
 @interface CreateGroupsViewController ()
 
@@ -45,9 +46,48 @@
     [[self view] endEditing:YES];
 }
 
+- (void)groupify {
+    
+}
+
 
 - (IBAction)createGroups:(id)sender {
-        //put textfields into NSIntegers and then groupify!
+    
+    NSInteger numberOfPeople = self.totalNumberOfPeople;
+    NSInteger numberOfGroups = [self.numberOfGroupsTF.text integerValue];
+    NSString *groupSetName = self.groupSetNameTF.text;
+    
+    NSMutableArray *shuffledNames = [[[PersonListStore sharedNameListStore] allNameLists] copy];
+    [shuffledNames ];
+    
+    func createGroups(numberOfGroups: Int, names: Array<String>) -> Array<Array<String>> {
+        
+        // Determine how many people in each group and how many are leftover
+        var amountInGroup:Int = Int(floor(Float(names.count) / Float(numberOfGroups)))
+        var amountRemaining:Int = names.count % numberOfGroups
+        
+        var shuffledNames = names.shuffleImmutable()
+        
+        // An array that contains all the group arrays
+        var listOfGroups:Array<Array<String>> = []
+        
+        // For each group
+        for (var i = 0; i < numberOfGroups; i++){
+            var group:Array<String> = []
+            
+            // Remove one name from arrayOfNames and add it to the current group
+            for (var j = 0; j < amountInGroup; j++) {
+                if (!shuffledNames.isEmpty) {
+                    group.append(shuffledNames.removeAtIndex(0))
+                }
+            }
+            listOfGroups.append(group)
+            
+        }
+        return listOfGroups
+        
+    }
+
     
 }
 
